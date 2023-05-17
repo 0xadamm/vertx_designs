d76efa9c97da947eda4f8ac451ca0b35b747f4bc
0xadamm <adam.mourade@gmail.com>
2023-05-17 12:11:55 -0700

    created projects

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,62 +1,148 @@
-// constants/projects.js
-
 export const PROJECTS = [
 	// Project 1
 	{
-		id: "project1",
-		category: ["all", "design"],
-		link: "/project1",
-		title: "Space Landing page",
+		id: "secretsnailsociety",
+		category: ["all", "design", "website"],
+		link: "/secretsnailsociety",
+		title: "Secret Snail Society",
 		tag: "Website",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. At officia ipsa obcaecati iure! Obcaecativitae sunt nulla totam repellendus voluptates    exercitationem non blanditiis provident quidem,dolore enim quam quae, voluptas maiores? Similiquequos excepturi itaque fugit veritatis laborenumquam. Praesentium saepe modi sequi animicorporis aperiam, ullam ipsum quasi natus corruptierror ad esse id rerum omnis quis ipsam adipisci?",
+			"4444 Secret Snails that live on the Ethereum blockchain are here to travel the multiverse of swamps Secret Snails believe in creating a unique environment and community while combining the physical and digital worlds in a way that enables all secret snails to to connect with each other create strong friendships ",
 
 		subdescription:
-			"Mosaic is a finance tool for making better businessmoves — automating aggregation, analysis, andfinancial forecasting. In 2021, we partnered to help them take charge of Strategic Finance category with a new brand, voiceexperience. To connect with their audience of leaders they needed to look and feel like a confident, no-nonsensebrand.",
+			"4444 Secret Snails that live on the Ethereum blockchain are here to travel the multiverse of swamps Secret Snails believe in creating a unique environment and community while combining the physical and digital worlds in a way that enables all secret snails to to connect with each other create strong friendships ",
 		thumbnail: {
-			src: "/assets/images/project-1.png",
+			src: "/assets/images/projects/secret_snail_society/sss.png",
 			alt: "project-1",
 			width: 350,
-			height: 466,
+			height: 350,
 		},
-		imagesrc1: "/assets/images/portfolio-img-1.png",
-		imagesrc2: "/assets/images/portfolio-img-2.png",
-		imagesrc3: "/assets/images/portfolio-img-3.png",
-		imagesrc4: "/assets/images/portfolio-img-4.png",
+		imagesrc1:
+			"/assets/images/projects/secret_snail_society/gallery.png",
+		imagesrc2:
+			"/assets/images/projects/secret_snail_society/discord.png",
+		imagesrc3:
+			"/assets/images/projects/secret_snail_society/sss.png",
+		imagesrc4:
+			"/assets/images/projects/secret_snail_society/roadmap.png",
 		features: [
-			"Lorem ipsum dolor sit amet consectetur",
-			"exercitationem non blanditiis provident",
-			"fugit veritatis laborenumquam. Praesentium",
-			"ullam ipsum quasi natus corruptierror ad",
+			"Minting Interface",
+			"Wallet Integration",
+			"Responsive Design",
+			"Royalties",
 		],
 	},
-	//Project 2
+	// Project 2
 	{
-		id: "project2",
-		category: ["all", "design"],
-		link: "/project2",
-		title: "Crypto Game",
+		id: "baddies",
+		category: ["all", "website"],
+		link: "/baddies",
+		title: "Blockchain Baddies",
 		tag: "Website",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. At officia ipsa obcaecati iure! Obcaecativitae sunt nulla totam repellendus voluptates    exercitationem non blanditiis provident quidem,dolore enim quam quae, voluptas maiores? Similiquequos excepturi itaque fugit veritatis laborenumquam. Praesentium saepe modi sequi animicorporis aperiam, ullam ipsum quasi natus corruptierror ad esse id rerum omnis quis ipsam adipisci?",
-
+			"The Blockchain Baddiues are the hottest NFTs on the blockchain. We're a collection of 10,000 unique NFTs who rule the under world off the ETH blockchain. Ownership includes creative/ commercial rights of your Baddie.",
 		subdescription:
-			"Mosaic is a finance tool for making better businessmoves — automating aggregation, analysis, andfinancial forecasting. In 2021, we partnered to help them take charge of Strategic Finance category with a new brand, voiceexperience. To connect with their audience of leaders they needed to look and feel like a confident, no-nonsensebrand.",
+			"The Blockchain Baddiues are the hottest NFTs on the blockchain. We're a collection of 10,000 unique NFTs who rule the under world off the ETH blockchain. Ownership includes creative/ commercial rights of your Baddie. ",
 		thumbnail: {
-			src: "/assets/images/project-3.png",
-			alt: "project-1",
+			src: "/assets/images/projects/baddies/baddies.png",
+			alt: "project-2",
 			width: 350,
-			height: 466,
+			height: 350,
 		},
-		imagesrc1: "/assets/images/portfolio-img-1.png",
-		imagesrc2: "/assets/images/portfolio-img-2.png",
-		imagesrc3: "/assets/images/portfolio-img-3.png",
-		imagesrc4: "/assets/images/portfolio-img-4.png",
+		imagesrc1: "/assets/images/projects/baddies/news.png",
+		imagesrc2: "/assets/images/projects/baddies/cta.png",
+		imagesrc3: "/assets/images/projects/baddies/baddies.png",
+		imagesrc4: "/assets/images/projects/baddies/roadmap.png",
 		features: [
-			"Lorem ipsum dolor sit amet consectetur",
-			"exercitationem non blanditiis provident",
-			"fugit veritatis laborenumquam. Praesentium",
-			"ullam ipsum quasi natus corruptierror ad",
+			"Minting Interface",
+			"Wallet Integration",
+			"Responsive Design",
+			"Royalties",
+		],
+	},
+	// Project 3
+	{
+		id: "cheetahgang",
+		category: ["all", "website"],
+		link: "/cheetahgang",
+		title: "Cheetah Gang Wine",
+		tag: "Website",
+		description:
+			"Forward to New Year 2030, and these fierce felines that were once agile have become lazy, slow and clumsy on the planet. And yes, these elegant animals are racing towards extinction on our planet. Very few cheetahs are left on the planet due to hunting by humans, climate change and habitat destruction. Due to these reasons, the population of cheetahs is decreasing day by day.",
+		subdescription:
+			"Forward to New Year 2030, and these fierce felines that were once agile have become lazy, slow and clumsy on the planet. And yes, these elegant animals are racing towards extinction on our planet. Very few cheetahs are left on the planet due to hunting by humans, climate change and habitat destruction. Due to these reasons, the population of cheetahs is decreasing day by day.",
+
+		thumbnail: {
+			src: "/assets/images/projects/cheetah_gang/cheetahs.png",
+			alt: "project-3",
+			width: 350,
+			height: 350,
+		},
+		imagesrc3: "/assets/images/projects/cheetah_gang/cheetahs.png",
+
+		features: [
+			"Minting Interface",
+			"Wallet Integration",
+			"Responsive Design",
+			"Royalties",
+		],
+	},
+	// Project 4
+	{
+		id: "personalPortfolio",
+		category: ["all", "website"],
+		link: "/personalPortfolio",
+		title: "Personal Portfolio",
+		tag: "Website",
+		description:
+			"In the dynamic intersection of design and technology, our CEO Adam has always found a deep fascination with the boundless potential of 3D elements. This passion is vividly reflected in his personal portfolio. Crafted using React Three Fiber, a robust react renderer for Three.js on the web, the portfolio is more than just a showcase of professional accomplishments and skills. It is a voyage into the captivating realm of 3D web design. Through this project, Adam aimed to deliver a unique user experience that blurs the lines between digital and physical, effectively demonstrating his proficiency in contemporary web technologies.",
+		subdescription:
+			"The process of building Adam's personal portfolio was both an enlightening journey and a series of challenges. The utilization of Blender and React Three Fiber put his adaptability and problem-solving skills to the test. Mastering the complex functionalities of Blender to create compelling 3D models was a task that demanded time and dedication. Following this, integrating these 3D models into a React application using React Three Fiber brought its own set of complexities. Rendering 3D models on the web, optimizing performance for smooth user interaction, and ensuring compatibility across various browsers and devices were among the hurdles encountered. Despite these challenges, overcoming them provided a valuable learning experience, expanding Adam's understanding of 3D web technologies and enriching his skill set as a developer. This journey ultimately led to the creation of a dynamic and unique online presence. ",
+		thumbnail: {
+			src: "/assets/images/projects/portfolio/hero.png",
+			alt: "project-4",
+			width: 350,
+			height: 350,
+		},
+		imagesrc1: "/assets/images/projects/portfolio/skills.png",
+		imagesrc2: "/assets/images/projects/portfolio/contact.png",
+		imagesrc3: "/assets/images/projects/portfolio/hero.png",
+		imagesrc4: "/assets/images/projects/portfolio/tech.png",
+
+		features: [
+			"React Three Fiber",
+			"Client Side Rendering",
+			"Interactive Elements",
+		],
+	},
+	// Project 5
+	{
+		id: "jobseeker",
+		category: ["all", "app"],
+		link: "/jobseeker",
+		title: "Job Seeker",
+		tag: "Mobile Application",
+		description:
+			"Leveraging the power of RapidAPI, Job Seeker aggregates job listings from numerous databases, making it easier than ever to find the perfect match for your career aspirations. Whether you're an entry-level graduate seeking your first position or an experienced professional looking for a career change, Job Seeker has the tools to make your job search process smooth and efficient.",
+		subdescription:
+			"Building Job Seeker was both a rewarding and challenging experience. One of the major challenges we faced was effectively working with RapidAPI for fetching and displaying data. As RapidAPI serves as a bridge to numerous job listing APIs, each with their unique structure and data format, the process of normalizing and harmonizing this disparate data into a unified format was a significant task.",
+		thumbnail: {
+			src: "/assets/images/projects/jobseeker/thumbnail1.png",
+			alt: "project-5",
+			width: 350,
+			height: 350,
+		},
+		imagesrc1: "/assets/images/projects/jobseeker/homepage.png",
+		imagesrc2: "/assets/images/projects/jobseeker/fulltime.png",
+		imagesrc3: "/assets/images/projects/jobseeker/about.png",
+		imagesrc4:
+			"/assets/images/projects/jobseeker/qualifications.png",
+
+		features: [
+			"React Native",
+			"Firebase",
+			"Cross Platform",
+			"Authentication",
 		],
 	},
 ];
