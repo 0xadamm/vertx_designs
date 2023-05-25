47fd76ac0f4a289cd0ee910863cc96a608f7602b
0xadamm <adam.mourade@gmail.com>
2023-05-25 16:40:03 -0700

    changed google analytics measurment id

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -27,7 +27,7 @@ const Portfoliodetail = () => {
 					<div className="container">
 						<div className="items-center py-10 md:flex md:h-[400px] md:py-0">
 							<div className="heading relative mb-0 md:w-[725px]">
-								<h4 className="!text-white">{project.title}</h4>
+								<h1 className="!text-white">{project.title}</h1>
 								<p className="relative mt-7 font-medium text-[#BBC0D0] before:absolute before:top-0 before:h-full before:w-1 before:bg-primary ltr:pl-8 ltr:before:left-0 rtl:pr-8 rtl:before:right-0">
 									{project.description}
 								</p>
